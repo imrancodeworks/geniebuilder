fceb047b34c7629b7c7869ff18737afbd3f2bfe6
imrancodeworks <imrancodeworks@gmail.com>
2026-04-07 16:13:36 +0530

    Upgrade pip in build script for better compatibility

--- render-build.sh
+++ render-build.sh
@@ -2,8 +2,12 @@
 # exit on error
 set -o errexit
 
+echo "--- Upgrading pip ---"
+python -m pip install --upgrade pip
+
 echo "--- Installing Python dependencies ---"
 pip install -r requirements.txt
+
 
 echo "--- Downloading spaCy model ---"
 python -m spacy download en_core_web_sm
